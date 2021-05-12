bd507cf12fe0535463941c945c12234b91a94a66
atila-oliveira <atilateioli@gmail.com>
2021-05-12 08:27:19 -0300

    Update jogo.js

--- jogo.js
+++ jogo.js
@@ -1,3 +1,5 @@
+var timerId = null;
+
 function iniciaJogo(){
     var url = window.location.search;
     var nivel_jogo = url.replace('?','');
@@ -16,11 +18,33 @@ function iniciaJogo(){
    document.getElementById("cronometro").innerHTML = tempo_segundos;
 
    
-   var qtd_baloes = 20;
+   var qtd_baloes = 80;
    criaBaloes(qtd_baloes);
 
    document.getElementById("baloes_inteiros").innerHTML = qtd_baloes;
    document.getElementById("baloes_estourados").innerHTML = 0;
+
+   contagemTempo(tempo_segundos + 1);
+}
+
+function contagemTempo(segundos){
+    segundos = segundos - 1;
+
+    if(segundos == -1){
+        clearTimeout(timerId);
+        gameOver();
+        return false;
+    }
+
+
+    document.getElementById("cronometro").innerHTML = segundos;
+    timerId = setTimeout("contagemTempo("+ segundos+ ")", 1000);
+
+}
+
+function gameOver(){
+    remove_eventos_baloes();
+    alert("Fim de jogo, você não conseguiu estourar todos os balões a tempo!")
 }
 
 function criaBaloes(qtd_baloes){
@@ -28,6 +52,52 @@ function criaBaloes(qtd_baloes){
         var balao = document.createElement("img");
         balao.src = 'imagens/balao_azul_pequeno.png';
         balao.style.margin = '10px';
+        balao.id = 'b' + i;
+        balao.onclick = function() {estourar(this)};
         document.getElementById("cenario").appendChild(balao)
+    }
+}
+
+function estourar(e){
+    var idBalao = e.id;
+    document.getElementById(idBalao).setAttribute("onclick","")
+    document.getElementById(idBalao).src = "imagens/balao_azul_pequeno_estourado.png";
+    pontuacao(-1);
+}
+
+function pontuacao(acao){
+    var baloesInteiros = document.getElementById('baloes_inteiros').innerHTML;
+    var baloesEstourados = document.getElementById('baloes_estourados').innerHTML;
+
+    baloesInteiros = parseInt(baloesInteiros);
+    baloesEstourados = parseInt(baloesEstourados);
+
+    baloesInteiros = baloesInteiros + acao;
+    baloesEstourados = baloesEstourados - acao;
+
+    document.getElementById("baloes_inteiros").innerHTML = baloesInteiros;
+    document.getElementById('baloes_estourados').innerHTML = baloesEstourados;
+
+    situacaoJogo(baloesInteiros);
+}
+
+function situacaoJogo(baloesInteiros){
+    if(baloesInteiros == 0){
+        alert("Parabéns, você conseguiu estourar todos os balões a tempo!");
+        pararJogo();
+    }
+}
+
+function pararJogo(){
+    clearTimeout(timerId);
+}
+
+function remove_eventos_baloes() {
+    var i = 1; 
+    
+    while(document.getElementById('b'+i)) {
+      
+        document.getElementById('b'+i).onclick = '';
+        i++; 
     }
 }
